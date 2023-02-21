f1dd2917b7a9fcb0d6d90ac378c6772f84ce318e
Moonshot <moonshot11@users.noreply.github.com>
2023-02-20 16:58:09 -0800

    Add source code

---
+++ source/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H3Patcher
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            const long BASE = 0x140000000;
+            var proc = new ManagedProcess("HITMAN3", scan: true);
+            proc.WriteNops(BASE + 0x0136D6A1, 6);
+        }
+    }
+}
